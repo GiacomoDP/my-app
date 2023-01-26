3e1bf48b698968caf5fa43a4698c2c0ca49310e7
Giacomo De Pol <giacomo.depol@hotmail.it>
2023-01-26 11:45:20 +0100

    react fragment

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function Greeting() {
-  return <h2>My First Component</h2>;
+  return (
+    <React.Fragment>
+      <div>
+        <h3>My First Component!!</h3>
+      </div>
+    </React.Fragment>
+  );
 }
+/*function Greeting() {
+  return React.createElement("h2", {}, "hello world");
+}*/
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
