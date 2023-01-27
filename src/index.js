8140d6890692815ac8126dc82ee88cce4983a6f2
Giacomo De Pol <giacomo.depol@hotmail.it>
2023-01-27 10:43:06 +0100

    addimg more css

--- src/index.js
+++ src/index.js
@@ -26,7 +26,12 @@ const Book = () => {
 const Image = () => <img src="/images/immagine prova.jpg"></img>;
 const Title = () => <h2>Spare</h2>;
 const Author = () => {
-  return <h4> Prince Harry The Duke of Sussex</h4>;
+  const inlineHeadingStyles = {
+    color: "#617d98",
+    fontSize: "0.75rem",
+    marginTop: "0.5rem",
+  };
+  return <h4 style={inlineHeadingStyles}>Prince Harry The Duke of Sussex</h4>;
 };
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
