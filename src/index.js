7fe721c344c81cf6d6a9ac3c6d51f61676faed31
Giacomo De Pol <giacomo.depol@hotmail.it>
2023-01-31 11:49:05 +0100

    simple list

--- src/index.js
+++ src/index.js
@@ -2,33 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
-const firstBook = {
-  author: "Prince Harry The Duke of Sussex",
-  title: "Spare",
-  img: "/images/immagine prova.jpg",
-};
+const books = [
+  {
+    author: "Prince Harry The Duke of Sussex",
+    title: "Spare",
+    img: "/images/immagine prova.jpg",
+  },
 
-const secondBook = {
-  author: "James Clear",
-  title: "Atomic Habits",
-  img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
-};
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
+  },
+];
+
+const names = ["john", "peter", "susan"];
+const newNames = names.map((name) => {
+  console.log(name);
+  return <h1>{name}</h1>;
+});
+console.log(newNames);
 
 const BookList = () => {
-  return (
-    <section className="booklist">
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      />
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.img}
-      />
-    </section>
-  );
+  return <section className="booklist">{newNames}</section>;
 };
 
 const Book = ({ img, title, author }) => {
