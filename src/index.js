7ddb162b810584396e1375c4ed1b529b38f7684c
Giacomo De Pol <giacomo.depol@hotmail.it>
2023-01-31 09:58:09 +0100

    props

--- src/index.js
+++ src/index.js
@@ -2,15 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
-const author = "Prince Harry The Duke of Sussex";
-const title = "Spare";
-const img = "/images/immagine prova.jpg";
+const firstBook = {
+  author: "Prince Harry The Duke of Sussex",
+  title: "Spare",
+  img: "/images/immagine prova.jpg",
+};
+
+const secondBook = {
+  author: "James Clear",
+  title: "Atomic Habits",
+  img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
+};
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book author={author} title={title} img={img} />
-      <Book author={author} title={title} img={img} />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
     </section>
   );
 };
