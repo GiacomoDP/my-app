1a63db5f16d0d372d7fa061bea84583d6a606546
Giacomo De Pol <giacomo.depol@hotmail.it>
2023-02-01 17:18:15 +0100

    key prop

--- src/index.js
+++ src/index.js
@@ -6,13 +6,15 @@ const books = [
   {
     author: "Prince Harry The Duke of Sussex",
     title: "Spare",
-    img: "/images/immagine prova.jpg",
+    img: "https://images-na.ssl-images-amazon.com/images/I/91Szm8FpdfL._AC_UL600_SR600,400_.jpg",
+    id: 1,
   },
 
   {
     author: "James Clear",
     title: "Atomic Habits",
     img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
+    id: 2,
   },
 ];
 
@@ -20,8 +22,8 @@ const BookList = () => {
   return (
     <section className="booklist">
       {books.map((book) => {
-        const { img, title, author } = book;
-        return <Book img={img} title={title} author={author} />;
+        const { img, title, author, id } = book;
+        return <Book img={img} title={title} author={author} key={id} />;
       })}
     </section>
   );
